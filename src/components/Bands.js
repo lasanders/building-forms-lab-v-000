2fa4e7218afc02c07b1211fa88056d2c1b4e7288
Lara Sanders <larasanders@gmail.com>
2019-07-28 20:08:30 +0000

    Automatically backed up by Learn

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 const Bands = props => {
+  const bands = props.bands.map((band, index) => {
+    return <li key={index}>{band.name}</li>;
+  });
+
   return (
-    <li>{props.text}</li>
+    <div>
+      {bands}
+    </div>
   );
+
 };
 
 export default Bands;
